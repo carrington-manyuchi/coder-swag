223d768b628b6772dbfc1387134ac69ed9bf6ebc
Carrington <Manyuchi>
2023-03-22 21:32:46 +0200

    Created second detailed VC for Products

---
+++ coder-swag/Views/ProductCell.swift
@@ -0,0 +1,19 @@
+//
+//  ProductCell.swift
+//  coder-swag
+//
+//  Created by Carrington Tafadzwa Manyuchi on 2023/03/22.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+    
+    
+    
+    
+}
